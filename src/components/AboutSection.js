efad0e55f8397422f17a174503e3d9045c88806f
Benjamin Steidl <localartpool@gmail.com>
2020-11-27 19:46:14 +0100

    Styled about section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
 import home1 from '../img/home1.png';
+
+const AboutSectionStyle = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  .description {
+    flex: 1;
+    padding-right: 5rem;
+    h2 {
+      font-weight: lighter;
+    }
+  }
+  .image {
+    flex: 1;
+    overflow: hidden;
+    img {
+      width: 100%;
+      height: 80vh;
+      object-fit: cover;   
+    }
+  }
+  .hide {
+    overflow: hidden;
+  }
+`;
 
 const AboutSection = () => {
   return(
-    <div>
+    <AboutSectionStyle>
       <div className="description">
         <div className="title">
           <div className="hide">
@@ -27,8 +55,10 @@ const AboutSection = () => {
         </p>
         <button>Contact us</button>
       </div>
-      <img src={home1} alt="Guy with a camera"/>
-    </div>
+      <div className="image">
+        <img src={home1} alt="Guy with a camera"/>
+      </div>
+    </AboutSectionStyle>
   )
 }
 
