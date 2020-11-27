d5ee84ff4c2a8e9a27d6797d497b87e4e978353f
Benjamin Steidl <localartpool@gmail.com>
2020-11-27 14:24:04 +0100

    Started with services section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
 
 const AboutUs = () => {
   return (
-    <div>
+    <>
       <AboutSection />
-    </div>
-  )
-}
+      <ServicesSection />
+    </>
+  );
+};
 
 export default AboutUs;
